f1a14e37d35b8c1ce8a70b60fec0993dbfbff364
Yehor Savchuk <95549799+YehorSk@users.noreply.github.com>
2024-10-30 19:06:14 +0100

    Configured frontend part

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-createApp(App).mount('#app')
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+app.use(vuetify);
+
+
+app.mount('#app');
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavComponent from "@/components/NavComponent.vue";
+</script>
+
+<template>
+  <NavComponent/>
+  <div class="p-4 sm:ml-64">
+    <h1>Hello World</h1>
+  </div>
+</template>
